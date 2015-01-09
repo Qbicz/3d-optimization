6f196f3f8ddb37347fa9dbb7ab89a95ce0412ba4
vergill27 <mietus27@interia.pl>
2015-01-09 14:28:26 +0100

    Update tabu.h

--- tabu.h
+++ tabu.h
@@ -3,13 +3,14 @@
 #define TABU_H_
 
 class tabu {
-	int size;
+	int size, Z; //Z-warstwa
 public:
-	int **tabu_array;// Ma trzy wiersze o d³ugoœci size(parametr optymalizacyjny), 0-X,1-Y,3-Z, przy sprawdzaniu ruchów na liœcie tabu bêdziemy brali uwagê co dwie s¹siednie pozycje
+	int **tabu_array;// Ma trzy wiersze o dÅ‚ugoÅ›ci size(parametr optymalizacyjny), 0-X,1-Y,3-Z, przy sprawdzaniu ruchÃ³w na liÅ›cie tabu bÄ™dziemy brali uwagÄ™ co dwie sÄ…siednie pozycje
 	tabu(int);
 	~tabu();
 	void display();
 	friend class board;
+	void updatetabu(int X0, int X1, int Y0, int Y1);
 };
 
 #endif
