fcf397cc9bd66afdcad59d333edf9a3f57eda59d
vergill27 <mietus27@interia.pl>
2015-01-09 14:41:56 +0100

    Update tabu.h

--- tabu.h
+++ tabu.h
@@ -5,7 +5,7 @@
 class tabu {
 	int size, Z; //Z-warstwa
 public:
-	int **tabu_array;// Ma trzy wiersze o długości size(parametr optymalizacyjny), 0-X,1-Y,3-Z, przy sprawdzaniu ruchów na liście tabu będziemy brali uwagę co dwie sąsiednie pozycje
+	int ***tabu_array;
 	tabu(int);
 	~tabu();
 	void display();
