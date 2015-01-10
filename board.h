05c4f811f1d3e2c27920b0731bede37527a14413
vergill27 <mietus27@interia.pl>
2015-01-10 11:04:48 +0100

    ehhh

--- board.h
+++ board.h
@@ -9,7 +9,7 @@ public:
 	char state;
 	friend class board;
 	point(){
-		state = '0';
+		state = '1';
 	}
 protected:
 
@@ -33,6 +33,7 @@ public:
 	void traceRouteFromVDec(VDec &Dec);
 	bool stop();
 	void jump(int x, int y, VDec &Dec1);
+	void ZnajdzKierunek(int x, int y, int z, VDec &Dec1);
 protected:
 
 };
